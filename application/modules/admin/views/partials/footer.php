e7182fb72bf6c87877c8377fa7678d9e34839ff0
Ali Raza <amr.shah@gmail.com>
2018-08-23 12:17:14 +0500

    Delete wrong files
    added by error

--- application/modules/admin/views/partials/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0.0
-    </div>
-    <strong>Copyright &copy; 2017-2018 <a href="https://amrshah.github.io" target="_blank">Ali Raza</a>.</strong> All rights reserved.
-  </footer>
