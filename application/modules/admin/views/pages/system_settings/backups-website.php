d0310513614fbb14709dd5e1f3f049422667bb06
Ali Raza <amr.shah@gmail.com>
2018-08-23 13:08:42 +0500

    views for settings, system settings

---
+++ application/modules/admin/views/pages/system_settings/backups-website.php
@@ -0,0 +1,48 @@
+ <?php include (dirname(dirname(dirname(__FILE__))) . '/partials/top-layout.php')?>
+
+  <!-- Content Wrapper. Contains this page's content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>Website Backup</h1>
+      <ol class="breadcrumb">
+        <li><a href="<?php echo $adminUrl?>"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">System</a></li>
+        <li class="active">website backup</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Website backup</h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                    title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fa fa-times"></i></button>
+          </div>
+        </div>
+        <div class="box-body">
+          Website backups Coming Soon!
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+          In-progress
+        </div>
+        <!-- /.box-footer-->
+      </div>
+      <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+ <?php include (dirname(dirname(dirname(__FILE__)))  . '/partials/bottom-layout.php')?>
